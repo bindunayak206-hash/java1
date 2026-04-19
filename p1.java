bc3b7d107a69666a534cdf93a33344f18541d5b0
bindunayak206-hash <bindunayak206@gmail.com>
2026-04-19 22:15:38 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.ArrayList;
+public class p1 {
+
+	public static void main(String[] args) 
+	{
+		List<String> ls = new ArrayList<>();
+		ls.add("Orange");
+		ls.add("geen");
+		ls.add("pink");
+		ls.add("red");
+		System.out.println(ls);
+		if(ls.contains("red")) {
+			System.out.println("available");
+		}else {
+			System.out.println("available not");
+		}
+	}
+
+}
